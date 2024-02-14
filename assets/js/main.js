cda9a320e202f2b5e2fe0ea6774bc6b0ce0ee66f
prasad-bigdp <bigdp585@gmail.com>
2024-02-14 23:28:29 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,13 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
 	 * Sticky header on scroll
 	 */
 	const selectHeader = document.querySelector("#header")
-	if (selectHeader) {
-		document.addEventListener("scroll", () => {
-			window.scrollY > 100
-				? selectHeader.classList.add("sticked")
-				: selectHeader.classList.remove("sticked")
-		})
-	}
+	selectHeader.classList.add('sticked')
+	// if (selectHeader) {
+	// 	document.addEventListener("scroll", () => {
+	// 		window.scrollY >= 0
+	// 			? selectHeader.classList.add("sticked")
+	// 			: selectHeader.classList.remove("sticked")
+	// 	})
+	// }
 
 	/**
 	 * Mobile nav toggle
